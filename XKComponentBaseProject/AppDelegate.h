902a51379f0f41c527c39b7bc79e60d9d8deb325
Tears <459834127@qq.com>
2019-12-05 14:13:52 +0800

    新增

---
+++ XKComponentBaseProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XKComponentBaseProject
+//
+//  Created by Jamesholy on 2019/2/27.
+//  Copyright © 2019 Jamesholy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+// test 11111
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
